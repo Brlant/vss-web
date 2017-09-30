4688a6165dbd17f94efbaae96ee20147816f6b49
zaoyu63 <zaoyu63@163.com>
2017-09-30 11:15:55 +0800

    实收权限

--- src/components/finance/receipt/list.vue
+++ src/components/finance/receipt/list.vue
@@ -172,9 +172,9 @@
                   {{row.createTime | date }}
                 </td>
                 <td>
-                  <!--<perm label="accounts-receivable-update">-->
+                  <perm label="accounts-receivable-update">
                   <a href="#" @click.stop.prevent="edit(row)"><i class="iconfont icon-edit"></i>增加实收金额</a>
-                  <!--</perm>-->
+                  </perm>
                 </td>
               </tr>
               </tbody>
