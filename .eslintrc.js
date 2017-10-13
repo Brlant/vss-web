16d9126b07f6405dbec97c5e71c2729775627aaf
zaoyu63 <zaoyu63@163.com>
2017-10-13 16:58:47 +0800

    eslint忽略未使用

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': [0, 'ignore'],
-    'indent': [0, 'ignore']
+    'indent': [0, 'ignore'],
+    'no-unused-vars': [0, 'ignore']
   }
 };
