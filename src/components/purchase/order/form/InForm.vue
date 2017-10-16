6fa8e17e25e05d55f3c8ea737dd8ae9eff36bddb
zaoyu63 <zaoyu63@163.com>
2017-10-16 14:39:22 +0800

    修改添加采购订单，查询货品的接口

--- src/components/purchase/order/form/InForm.vue
+++ src/components/purchase/order/form/InForm.vue
@@ -813,16 +813,14 @@
         });
       },
       searchProduct: function (query) {
-        if (!this.form.orgId || !this.form.supplierId) {
+        if (!this.form.supplierId) {
           this.searchProductList = [];
           return;
         }
         let params = {
-          orgId: this.form.orgId,
-          keyWord: query,
-          factoryId: this.form.supplierId
+          keyWord: query
         };
-        http.get('/org/goods/valid', {params: params}).then(res => {
+        http.get(`purchase-agreement/${this.form.supplierId}/valid/org-goods`, {params: params}).then(res => {
           this.searchProductList = res.data.list;
           this.$nextTick(function () {
             this.filterProducts();
