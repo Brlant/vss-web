e2c2666622c1cdf307e8bca2821b3ef946c28681
zaoyu63 <zaoyu63@163.com>
2017-10-25 18:31:11 +0800

    修改组合货品

--- src/components/resource/vaccin/data/form/form.vue
+++ src/components/resource/vaccin/data/form/form.vue
@@ -198,9 +198,24 @@
       <div v-show="form.goodsIsCombination">
         <!--<el-form ref="otherGoodsForm" :model="form" :rules="otherGoodsRules" label-width="120px">-->
         <el-form-item label="其他组织疫苗">
-          <el-select placeholder="请选择组织疫苗" v-model="otherForm.accessory" filterable popper-class="good-selects">
+          <el-select placeholder="请选择组织疫苗" v-model="otherForm.accessory" filterable popper-class="good-selects" remote
+                     :remote-method="getCombinationGoods" clearable="true">
             <el-option :label="item.orgGoodsDto.name" :value="item.orgGoodsDto.id" :key="item.orgGoodsDto.id"
                        v-for="item in otherGoodsList">
+              <div style="overflow: hidden">
+                <span class="pull-left">{{item.orgGoodsDto.name}}</span>
+              </div>
+              <div style="overflow: hidden">
+              <span class="select-other-info pull-left"><span
+                v-show="item.orgGoodsDto.goodsId">平台货品ID</span>  {{item.orgGoodsDto.goodsId}}
+              </span>
+                <span class="select-other-info pull-left"><span
+                  v-show="item.orgGoodsDto.goodsNo">货品编号</span>  {{item.orgGoodsDto.goodsNo}}
+              </span>
+                <span class="select-other-info pull-left"><span
+                  v-show="item.orgGoodsDto.salesFirmName">供货厂商</span>  {{ item.orgGoodsDto.salesFirmName }}
+              </span>
+              </div>
             </el-option>
           </el-select>
         </el-form-item>
@@ -325,6 +340,7 @@
           this.form.bidPrice = utils.autoformatDecimalPoint(this.form.bidPrice.toString());
           this.form.procurementPrice = utils.autoformatDecimalPoint(this.form.procurementPrice.toString());
           this.form.sellPrice = utils.autoformatDecimalPoint(this.form.sellPrice.toString());
+          this.otherGoodsList = [];
         } else {
           this.getCombinationGoods();
           this.getOmsGoods();
@@ -345,6 +361,7 @@
             inventoryLowerLimit: null,
             inventoryUpperLimit: null
           };
+          this.otherGoodsList = [];
         }
         this.otherForm = {
           accessory: '',
@@ -425,7 +442,7 @@
         });
         Vaccine.query(params).then(res => {
           this.invariantOtherGoodslist = JSON.parse(JSON.stringify(res.data.list));
-          this.filterSelectGoodsList(res.data.list);
+          this.filtersCombinationGoods();
         });
       },
       getGoodsType: function (id) {
