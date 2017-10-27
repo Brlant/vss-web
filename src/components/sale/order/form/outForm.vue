2a317a97e08025e64c891ae645223ff1c966b17f
zaoyu63 <zaoyu63@163.com>
2017-10-27 19:25:21 +0800

    出库去掉是否同批号，销售去掉是否进口

--- src/components/sale/order/form/outForm.vue
+++ src/components/sale/order/form/outForm.vue
@@ -293,10 +293,10 @@
             <el-form-item label="实际收货人" v-show="showContent.isShowOtherContent">
               <oms-input type="text" placeholder="请输入实际收货人" v-model="form.actualConsignee"></oms-input>
             </el-form-item>
-            <el-form-item label="是否同批号">
-              <el-switch on-text="是" off-text="否" on-color="#13ce66" off-color="#ff4949"
-                         v-model="form.sameBatchNumber"></el-switch>
-            </el-form-item>
+            <!--<el-form-item label="是否同批号">-->
+            <!--<el-switch on-text="是" off-text="否" on-color="#13ce66" off-color="#ff4949"-->
+            <!--v-model="form.sameBatchNumber"></el-switch>-->
+            <!--</el-form-item>-->
             <el-form-item label="疾控仓库地址" prop="logisticsCentreId">
               <!--<el-select placeholder="请选择物流中心" v-model="form.logisticsCentreId" filterable :clearable="true">-->
               <!--<el-option :label="item.name" :value="item.id" :key="item.id" v-for="item in LogisticsCenter"/>-->
@@ -314,10 +314,10 @@
                            v-for="item in transportationConditionList"></el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="是否进口">
-              <el-switch on-text="是" off-text="否" on-color="#13ce66" off-color="#ff4949"
-                         v-model="form.importedFlag"></el-switch>
-            </el-form-item>
+            <!--<el-form-item label="是否进口">-->
+            <!--<el-switch on-text="是" off-text="否" on-color="#13ce66" off-color="#ff4949"-->
+            <!--v-model="form.importedFlag"></el-switch>-->
+            <!--</el-form-item>-->
             <el-form-item :label="'预计送货时间'"
                           :prop=" showContent.isShowOtherContent?'expectedTime':'' "
                           v-show="showContent.isShowOtherContent">
