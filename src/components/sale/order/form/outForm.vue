d27657918fb69add7752a33293f40e555a9bc6db
zaoyu63 <zaoyu63@163.com>
2017-11-20 10:51:57 +0800

    修改出库仓库地址字段

--- src/components/sale/order/form/outForm.vue
+++ src/components/sale/order/form/outForm.vue
@@ -297,11 +297,11 @@
             <!--<el-switch on-text="是" off-text="否" on-color="#13ce66" off-color="#ff4949"-->
             <!--v-model="form.sameBatchNumber"></el-switch>-->
             <!--</el-form-item>-->
-            <el-form-item label="疾控仓库地址" prop="logisticsCentreId">
-              <!--<el-select placeholder="请选择物流中心" v-model="form.logisticsCentreId" filterable :clearable="true">-->
+            <el-form-item label="疾控仓库地址" prop="orgAddress">
+              <!--<el-select placeholder="请选择物流中心" v-model="form.orgAddress" filterable :clearable="true">-->
               <!--<el-option :label="item.name" :value="item.id" :key="item.id" v-for="item in LogisticsCenter"/>-->
               <!--</el-select>-->
-              <el-select placeholder="请选择疾控仓库地址" v-model="form.logisticsCentreId" filterable :clearable="true">
+              <el-select placeholder="请选择疾控仓库地址" v-model="form.orgAddress" filterable :clearable="true">
                 <el-option :label="item.name" :value="item.id" :key="item.id" v-for="item in LogisticsCenter">
                   <span class="pull-left">{{ item.name }}</span>
                   <span class="pull-right" style="color: #999">{{ getWarehouseAdress(item) }}</span>
@@ -598,7 +598,7 @@
           transportationAddress: '',
           importedFlag: false,
           orgRelation: '',
-          logisticsCentreId: '',
+          orgAddress: '',
           sameBatchNumber: false,
           actualConsignee: '',
           'thirdPartyNumber': '',
@@ -633,7 +633,7 @@
           logisticsProviderId: [
             {required: true, message: '请选择物流商', trigger: 'change'}
           ],
-          logisticsCentreId: [
+          orgAddress: [
             {required: true, message: '请选择疾控仓库地址', trigger: 'change'}
           ],
           transportationCondition: [
@@ -759,8 +759,8 @@
 //      let oldForm = window.localStorage.getItem(this.saveKey);
 //      if (oldForm) {
 //        this.form = Object.assign({}, this.form, JSON.parse(oldForm));
-//        this.form.logisticsCentreId = this.form.logisticsCentreId
-//          ? this.form.logisticsCentreId : window.localStorage.getItem('logisticsCentreId');
+//        this.form.orgAddress = this.form.orgAddress
+//          ? this.form.orgAddress : window.localStorage.getItem('orgAddress');
 //      }
     },
     methods: {
@@ -782,7 +782,7 @@
         this.form.customerId = '';
         this.form.transportationAddress = '';
         this.form.actualConsignee = '';
-        this.form.logisticsCentreId = '';
+        this.form.orgAddress = '';
       },
       editOrderInfo () {
         if (!this.orderId) return;
@@ -944,7 +944,7 @@
         }).then(res => {
           this.LogisticsCenter = res.data;
           let defaultStore = res.data.filter(item => item.default);
-          this.form.logisticsCentreId = defaultStore.length ? defaultStore[0].id : '';
+          this.form.orgAddress = defaultStore.length ? defaultStore[0].id : '';
         });
       },
       changeWarehouseAdress: function (val) {
