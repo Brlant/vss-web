3aa8bd1fd03da187a2fe8d102b5e8e55f9afc57f
zaoyu63 <zaoyu63@163.com>
2017-11-30 21:59:03 +0800

    采购协议，编辑时，不取货品的采购价格

--- src/components/resource/vaccin/purchaseAgreement/form/form.vue
+++ src/components/resource/vaccin/purchaseAgreement/form/form.vue
@@ -137,14 +137,16 @@
     },
     methods: {
       setSalesFirmName: function (item) {
-        if (item) {
-          this.goodsList.forEach(val => {
-            if (val.orgGoodsDto.id === item) {
-              this.salesFirmName = val.orgGoodsDto.salesFirmName;
-              this.form.supplyCompanyId = val.orgGoodsDto.salesFirm;
-              this.form.unitPrice = utils.autoformatDecimalPoint(val.orgGoodsDto.procurementPrice.toString());
-            }
-          });
+        if (this.action !== 'edit') {
+          if (item) {
+            this.goodsList.forEach(val => {
+              if (val.orgGoodsDto.id === item) {
+                this.salesFirmName = val.orgGoodsDto.salesFirmName;
+                this.form.supplyCompanyId = val.orgGoodsDto.salesFirm;
+                this.form.unitPrice = utils.autoformatDecimalPoint(val.orgGoodsDto.procurementPrice.toString());
+              }
+            });
+          }
         }
       },
       getOmsGoods: function (keyWord) {// 得到组织疫苗列表
