126555ea32b5fe1945152ff6c478bca2efebaa18
zaoyu63 <zaoyu63@163.com>
2018-01-08 15:34:01 +0800

    编辑订单货品时，可用库存不存在时，对应数据加回去

--- src/components/sale/order/form/batchNumber.vue
+++ src/components/sale/order/form/batchNumber.vue
@@ -180,36 +180,48 @@
         this.batchNumbers.forEach(i => {
           if (i.orgGoodsId === this.editItemProduct.orgGoodsId) {
             if (!i.lots.length) {
-              i.lots.push({
-                id: this.editItemProduct.batchNumberId,
-                no: this.editItemProduct.no,
-                inEffectiveFlag: this.editItemProduct.inEffectiveFlag,
-                productCount: this.editItemProduct.amount,
-                productionDate: this.editItemProduct.productionDate,
-                expirationDate: this.editItemProduct.expiryDate,
-                isChecked: true
-              });
-              // 编辑时，如果是已经保存的货品，取其相应的数量当可用库存
-              this.formCopy.detailDtoList.forEach(p => {
-                if (p.orgGoodsId === this.editItemProduct.orgGoodsId) {
-                  i.lots[0].count = p.amount;
-                }
-              });
+              this.addBatchNumber(i);
             } else {
               // 把对应的数量，重新加回去
-              i.lots.forEach(i => {
-                if (i.id === this.editItemProduct.batchNumberId) {
-                  i.productCount = this.editItemProduct.amount;
-                  // 编辑时，如果是已经保存的货品，取其相应的数量+还剩下的可用库存
-                  this.formCopy.detailDtoList.forEach(p => {
-                    if (p.orgGoodsId === this.editItemProduct.orgGoodsId) {
-                      i.count = i.count + p.amount;
-                    }
-                  });
-                  i.isChecked = true;
-                }
-              });
+              let isHas = i.lots.some(s => s.id === this.editItemProduct.batchNumberId);
+              if (isHas) {
+                i.lots.forEach(i => {
+                  if (i.id === this.editItemProduct.batchNumberId) {
+                    i.productCount = this.editItemProduct.amount;
+                    // 编辑时，如果是已经保存的货品，取其相应的数量+还剩下的可用库存
+                    this.formCopy.detailDtoList.forEach(p => {
+                      if (p.batchNumberId === this.editItemProduct.batchNumberId) {
+                        i.count = i.count + p.amount;
+                      }
+                    });
+                    i.isChecked = true;
+                  }
+                });
+              } else {
+                this.addBatchNumber(i);
+              }
             }
+          }
+        });
+      },
+      addBatchNumber (i) {
+        i.lots.push({
+          id: this.editItemProduct.batchNumberId,
+          no: this.editItemProduct.no,
+          inEffectiveFlag: this.editItemProduct.inEffectiveFlag,
+          productCount: this.editItemProduct.amount,
+          productionDate: this.editItemProduct.productionDate,
+          expirationDate: this.editItemProduct.expiryDate,
+          isChecked: true
+        });
+        // 编辑时，如果是已经保存的货品，取其相应的数量当可用库存
+        this.formCopy.detailDtoList.forEach(p => {
+          if (p.batchNumberId === this.editItemProduct.batchNumberId) {
+            i.lots.forEach(il => {
+              if (il.id === this.editItemProduct.batchNumberId) {
+                il.count = p.amount;
+              }
+            });
           }
         });
       },
