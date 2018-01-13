aeae6c1fbe189a45a5355d7f391bb5887391df64
zaoyu63 <zaoyu63@163.com>
2018-01-13 13:18:59 +0800

    获取角色权限方法

--- src/tools/menu.js
+++ src/tools/menu.js
@@ -1,19 +1,22 @@
 import {Access} from '@/resources.js';
 
-export default {
-  getRoleMenus(vm) {
-    let menuList = vm.$store.state.permList;
-    if (menuList) {
-      return menuList;
-    }
-    let userId = vm.$store.state.user.userCompanyAddress;
-    if (!userId) {
-      return;
-    }
-    Access.getRoleMenus(userId).then(res => {
-      let menuData = res.data;
-      vm.$store.commit('initPermList', menuData);
-      menuList = menuData;
-    });
+/**
+ * 得到登录用户权限，放到vuex，并给当前vue实例添加一个属性,存储用户权限
+ * @param vm
+ * @param property
+ */
+export function getRoleMenus(vm, property) {
+  let menu = vm.$store.state.permList;
+  if (menu && menu.menuList) {
+    vm[property] = menu;
+    return;
   }
-};
+  let userId = vm.$store.state.user.userCompanyAddress;
+  if (!userId) {
+    return;
+  }
+  Access.getRoleMenus(userId).then(res => {
+    vm.$store.commit('initPermList', res.data);
+    vm[property] = res.data;
+  });
+}
