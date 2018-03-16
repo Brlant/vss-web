d19e7f8bbb079ccedafd89dcdcf38043aa8ee5ba
zhouhao <798804880@qq.com>
2018-03-16 09:44:22 +0800

    修改新增合同提示信息出错的bug

--- src/components/purchase/contract/form/InForm.vue
+++ src/components/purchase/contract/form/InForm.vue
@@ -958,7 +958,7 @@
           });
           this.doing = true;
           if (saveData.bizType > 1) saveData.supplierId = saveData.orgId;
-          if (saveData.id) {
+          if (saveData.id && this.action === 'edit') {
             PurchaseContract.updateOrder(saveData.id, saveData).then(res => {
               this.resetForm();
               this.$notify({
