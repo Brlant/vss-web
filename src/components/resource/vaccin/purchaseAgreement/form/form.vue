2c2dd5fc6e1a894166c4638ab0e0706b318f7f23
zhouhao <798804880@qq.com>
2018-05-23 15:26:43 +0800

    二类疫苗采购目录疫苗新增供货厂商字段

--- src/components/resource/vaccin/purchaseAgreement/form/form.vue
+++ src/components/resource/vaccin/purchaseAgreement/form/form.vue
@@ -11,12 +11,12 @@
             <span class="pull-left">{{item.orgGoodsDto.name}}</span>
           </div>
           <div style="overflow: hidden">
-                      <span class="select-other-info pull-left"><span
-                        v-show="item.orgGoodsDto.goodsNo">货品编号:</span>{{item.orgGoodsDto.goodsNo}}
-                      </span>
-            <span class="select-other-info pull-left"><span
-              v-show="item.orgGoodsDto.salesFirmName">供货厂商:</span>{{ item.orgGoodsDto.salesFirmName }}
-                      </span>
+              <span class="select-other-info pull-left"><span
+                v-show="item.orgGoodsDto.goodsNo">货品编号:</span>{{item.orgGoodsDto.goodsNo}}
+              </span>
+              <span class="select-other-info pull-left"><span
+                v-show="item.orgGoodsDto.salesFirmName">供货厂商:</span>{{ item.orgGoodsDto.salesFirmName }}
+              </span>
           </div>
         </el-option>
       </el-select>
@@ -55,7 +55,7 @@
   </el-form>
 </template>
 <script>
-  import { PurchaseAgreement, Vaccine } from '../../../../../resources';
+  import {PurchaseAgreement, Vaccine} from '../../../../../resources';
   import utils from '../../../../../tools/utils';
 
   export default {
