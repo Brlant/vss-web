024bfc4daf33f6c9a37e39758d29ade87d0adcc1
zhouhao <798804880@qq.com>
2018-08-21 13:42:45 +0800

    修改市药监购进和销售数据页面

--- src/components/report/drug-administration-out.vue
+++ src/components/report/drug-administration-out.vue
@@ -30,8 +30,8 @@
                 <el-col :span="24">
                   <el-date-picker
                     v-model="bizDateAry"
-                    type="datetimerange"
-                    placeholder="请选择业务日期" :default-time="['00:00:00', '23:59:59']">
+                    type="daterange"
+                    placeholder="请选择" format="yyyy-MM-dd">
                   </el-date-picker>
                 </el-col>
               </oms-form-row>
@@ -92,14 +92,14 @@
       exportFile: function () {
         let moduleId = this.$route.meta.type === '0' ? '/drug-administration/report/in' : '/drug-administration/report/out';
         let url = this.$route.meta.type === '0' ? '/erp-statement/drug-administration/in-warehouse/export' : '/erp-statement/drug-administration/out-warehouse/export';
-        this.searchWord.createStartTime = this.formatTime(this.bizDateAry ? this.bizDateAry[0] : '');
-        this.searchWord.createEndTime = this.formatTime(this.bizDateAry ? this.bizDateAry[1] : '');
+        this.searchWord.createStartTime = this.formatTime(this.bizDateAry ? this.bizDateAry[0] : '') + ' ' + '00:00:00';
+        this.searchWord.createEndTime = this.formatTime(this.bizDateAry ? this.bizDateAry[1] : '') + ' ' + '23:59:59';
         this.searchWord.type = this.$route.meta.type;
         let params = Object.assign({}, this.searchWord);
         this.isLoading = true;
         this.$store.commit('initPrint', {isPrinting: true, moduleId: moduleId});
         this.$http.get(url, {params}).then(res => {
-          let fileName = this.$route.meta.type === '0' ? '购进数据' : '销售数据';
+          let fileName = this.$route.meta.type === '0' ? '购进数据.xml' : '销售数据.xml';
           if (this.searchWord.createStartTime && this.searchWord.createEndTime) {
             fileName = this.searchWord.createStartTime + '至' + this.searchWord.createEndTime + fileName;
           }
@@ -123,7 +123,7 @@
         this.bizDateAry = '';
       },
       formatTime: function (date) {
-        return date ? this.$moment(date).format('YYYY-MM-DD HH:mm:ss') : '';
+        return date ? this.$moment(date).format('YYYY-MM-DD') : '';
       }
     }
   };
