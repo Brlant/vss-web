93521294e7d2555b8d9d6f9c3865415acb7c50a9
zhouhao <798804880@qq.com>
2018-08-31 11:25:55 +0800

    附件名称可编辑

--- src/components/common/attachmentList.vue
+++ src/components/common/attachmentList.vue
@@ -29,15 +29,25 @@
 </style>
 <template>
   <ul class="show-list">
-    <li class="list-item list_flex" v-for="attachment in attachmentList" @click="handlePreview(attachment)">
-      <div class="attachment-name">
+    <li class="list-item list_flex" v-for="attachment in attachmentList"
+        :style="{'line-height':attachment.showEdit?'50px':'25px'}">
+      <div class="attachment-name" v-show="!attachment.showEdit" @click="handlePreview(attachment)">
         {{attachment.attachmentFileName}}
+      </div>
+      <div v-show="attachment.showEdit">
+        <el-input v-model="attachment.attachmentFileName" placeholder="请输入附件名称" autosize
+                  @blur="editAttachmentName(attachment)"></el-input>
       </div>
       <div>
         <perm :label="perm">
           <a :href="attachment.attachmentStoragePath "
              class="download-link pull-right" :download="attachment.attachmentFileName" @click.stop="">
             <i class="el-icon-t-download"></i>
+          </a>
+        </perm>
+        <perm label="erp-attachment-name-update">
+          <a href="#" class="download-link pull-right" @click.stop.prevent="editName(attachment)">
+            <i class="el-icon-t-edit"></i>
           </a>
         </perm>
         <perm :label="deletePermission">
@@ -80,6 +90,40 @@
     },
     props: ['objectId', 'objectType', 'attachmentIdList', 'permission', 'deletePermission'],
     methods: {
+      editAttachmentName: function (item) {
+        if (item && !item.attachmentFileName) {
+          this.$notify.warning({
+            duration: 2000,
+            message: '附件文件名不能为空！'
+          });
+          return;
+        }
+        // 修改附件名称
+        let param = {'attachmentFileName': item.attachmentFileName};
+        let index = this.attachmentList.indexOf(item);
+        this.$http.put(`/omsAttachment/${item.attachmentId}/name`, param).then(res => {
+          this.$notify.success({
+            message: '修改附件文件名成功'
+          });
+          this.attachmentList.splice(index, 1, res.data);
+        }).catch(error => {
+          this.$notify.error({
+            message: error.response.data && error.response.data.msg || '修改附件文件名失败'
+          });
+        });
+      },
+      editName: function (val) {
+        if (val && !val.attachmentFileName) {
+          this.$notify.warning({
+            duration: 2000,
+            message: '附件文件名不能为空！'
+          });
+          return;
+        }
+        let index = this.attachmentList.indexOf(val);
+        val.showEdit = !val.showEdit;
+        this.attachmentList.splice(index, 1, val);
+      },
       handleRemove (attachment) {
         if (!attachment) {
           return;
@@ -108,6 +152,9 @@
         if (!this.object.objectId) return;
         OmsAttachment.queryOneAttachmentList(this.object.objectId, this.object.objectType).then(res => {
           this.attachmentList = res.data;
+          this.attachmentList.forEach(val => {
+            val.showEdit = false;
+          });
         });
       },
       handlePreview (file) {
