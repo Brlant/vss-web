f06084d126daa86af21cb06e0079b88545b25313
zaoyu63 <zaoyu63@163.com>
2018-10-26 17:34:05 +0800

    报损去掉待收货

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -67,6 +67,14 @@ export default {
     4: {'title': '已完成', state: '4', num: ''},
     5: {'title': '已取消', state: '5', num: ''}
   },
+  outBreakageOrderType: {
+    0: {'title': '待确认', state: '0', num: ''},
+    1: {'title': '待审单', state: '1', num: ''},
+    2: {'title': '执行中', state: '2', num: ''},
+    // 3: {'title': '待收货', state: '3', num: ''},
+    4: {'title': '已完成', state: '4', num: ''},
+    5: {'title': '已取消', state: '5', num: ''}
+  },
   outReturnOrderType: {
     0: {'title': '待确认', state: '0', num: ''},
     1: {'title': '待审单', state: '1', num: ''},
