bdaa131a2d16dbceb52a461c2085ed53a20554ad
zhouhao <798804880@qq.com>
2018-12-20 16:24:35 +0800

    删除多余空格

--- src/components/files/vaccineBatch/index.vue
+++ src/components/files/vaccineBatch/index.vue
@@ -240,8 +240,6 @@
 //        Object.assign(this.filters, temp);
       },
       filterFactory(query) { // 查询厂商
-
-
         let orgId = this.$store.state.user.userCompanyAddress;
         if (!orgId) {
           return;
