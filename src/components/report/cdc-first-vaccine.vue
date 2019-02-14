93dc1d921ccfbd439ce673bcea007bd324b3f1a2
zaoyu63 <zaoyu63@163.com>
2019-02-14 11:49:05 +0800

    去掉一类苗领用记录, 关联单位的搜索

--- src/components/report/cdc-first-vaccine.vue
+++ src/components/report/cdc-first-vaccine.vue
@@ -16,32 +16,32 @@
         </div>
         <el-form class="advanced-query-form">
           <el-row>
-            <el-col :span="7" class="clearfix">
+            <el-col :span="6" class="clearfix">
               <oms-form-row label="区县" :span="6">
                 <oms-input type="text" v-model="searchWord.areaCode" placeholder="请输入区县"></oms-input>
               </oms-form-row>
             </el-col>
+            <!--<el-col :span="8">-->
+            <!--<oms-form-row label="关联单位" :span="6">-->
+            <!--<el-select filterable remote placeholder="请输入关联单位查询" :remote-method="filterRelation"-->
+            <!--:clearable="true" :loading="selectLoading"-->
+            <!--v-model="searchWord.customerId" @click.native.once="filterRelation('')"-->
+            <!--popperClass="good-selects">-->
+            <!--<el-option :value="org.id" :key="org.id" :label="org.name" v-for="org in orgList">-->
+            <!--<div style="overflow: hidden">-->
+            <!--<span class="pull-left" style="clear: right">{{org.name}}</span>-->
+            <!--</div>-->
+            <!--<div style="overflow: hidden">-->
+            <!--<span class="select-other-info pull-left">-->
+            <!--<span>系统代码:</span>{{org.manufacturerCode}}-->
+            <!--</span>-->
+            <!--</div>-->
+            <!--</el-option>-->
+            <!--</el-select>-->
+            <!--</oms-form-row>-->
+            <!--</el-col>-->
             <el-col :span="8">
-              <oms-form-row label="关联单位" :span="6">
-                <el-select filterable remote placeholder="请输入关联单位查询" :remote-method="filterRelation"
-                           :clearable="true" :loading="selectLoading"
-                           v-model="searchWord.customerId" @click.native.once="filterRelation('')"
-                           popperClass="good-selects">
-                  <el-option :value="org.id" :key="org.id" :label="org.name" v-for="org in orgList">
-                    <div style="overflow: hidden">
-                      <span class="pull-left" style="clear: right">{{org.name}}</span>
-                    </div>
-                    <div style="overflow: hidden">
-                      <span class="select-other-info pull-left">
-                        <span>系统代码:</span>{{org.manufacturerCode}}
-                      </span>
-                    </div>
-                  </el-option>
-                </el-select>
-              </oms-form-row>
-            </el-col>
-            <el-col :span="9">
-              <oms-form-row label="业务日期" :span="4">
+              <oms-form-row label="业务日期" :span="6">
                 <el-col :span="24">
                   <el-date-picker
                     v-model="bizDateAry"
@@ -51,8 +51,6 @@
                 </el-col>
               </oms-form-row>
             </el-col>
-          </el-row>
-          <el-row>
             <el-col :span="10">
               <oms-form-row label="产品名称" :span="4">
                 <el-select filterable remote placeholder="请输入产品名称" :remote-method="filterOrgGoods"
@@ -80,12 +78,13 @@
                 </el-select>
               </oms-form-row>
             </el-col>
-            <el-col :span="2"></el-col>
+          </el-row>
+          <el-row>
             <el-col :span="12">
-              <oms-form-row label="供/收货单位名称" :span="5">
+              <oms-form-row label="供/收货单位名称" :span="7">
                 <el-select filterable remote placeholder="请输入供/收货单位名称查询"
                            :remote-method="filterProvide" :clearable="true" :loading="selectLoading"
-                           v-model="searchWord.factoryId" @click.native.once="filterProvide('')"
+                           v-model="searchWord.customerId" @click.native.once="filterProvide('')"
                            popperClass="good-selects">
                   <el-option :value="org.id" :key="org.id" :label="org.name" v-for="org in provideList">
                     <div style="overflow: hidden">
@@ -100,9 +99,7 @@
                 </el-select>
               </oms-form-row>
             </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8">
+            <el-col :span="12">
               <oms-form-row label="批号" :span="5">
                 <el-select v-model="searchWord.batchNumberId" filterable clearable remote
                            :remoteMethod="filterBatchNumber" placeholder="请输入批号名称搜索批号"
@@ -112,8 +109,10 @@
                 </el-select>
               </oms-form-row>
             </el-col>
-            <el-col :span="8">
-              <oms-form-row label="" :span="3">
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <oms-form-row label="" :span="1">
                 <perm label="cdc-free-vaccine-sale-manager-export">
                   <el-button type="primary" @click="search" :disabled="loadingData">
                     查询
