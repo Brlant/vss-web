334ab2c62136a47a400d4856e464abc8e00ae79b
zaoyu63 <zaoyu63@163.com>
2019-05-06 15:40:58 +0800

    系统角色管理权限菜单前台提供, 后台不在提供

--- src/mixins/roleMixin.js
+++ src/mixins/roleMixin.js
@@ -1,4 +1,5 @@
 import {Access} from '@/resources';
+import menuTree from '@/components/account/system/menu';
 
 export default {
   mounted() {
@@ -40,31 +41,32 @@ export default {
     getOmsRoleMenus(noCache = false) {
       return new Promise((resolve, reject) => {
         let menu = this.$store.state.allMenuList;
-        if (noCache === false && menu && menu.length) {
+        if (noCache === false && menu && menu.data.length) {
           resolve(menu);
         } else {
           let userId = this.$store.state.user.userCompanyAddress;
           if (!userId) {
             reject('no user');
           } else {
-            Access.getOmsRoleMenus().then(res => {
-              this.$store.commit('initPermList', JSON.parse(JSON.stringify(res)));
-              resolve(res);
-              let getParentIds = (menus, parentsIds) => {
-                menus.forEach(i => {
-                  if (i.children) {
-                    parentsIds.push(i.id);
-                    getParentIds(i.children, parentsIds);
-                  }
-                });
-              };
-              let setParentIds = (menus) => {
-                let parentIds = [];
-                getParentIds(menus, parentIds);
-                this.$store.commit('initMenuParentIds', parentIds);
-              };
-              setParentIds(res.data);
-            });
+            let res = {
+              data: menuTree
+            };
+            this.$store.commit('initPermList', JSON.parse(JSON.stringify(res)));
+            resolve(res);
+            let getParentIds = (menus, parentsIds) => {
+              menus.forEach(i => {
+                if (i.children) {
+                  parentsIds.push(i.id);
+                  getParentIds(i.children, parentsIds);
+                }
+              });
+            };
+            let setParentIds = (menus) => {
+              let parentIds = [];
+              getParentIds(menus, parentIds);
+              this.$store.commit('initMenuParentIds', parentIds);
+            };
+            setParentIds(res.data);
           }
         }
       });
