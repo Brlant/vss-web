f071d5bc6a7c4861b4eda03b01e932e437349295
zaoyu63 <zaoyu63@163.com>
2019-07-20 16:21:27 +0800

    修改推送错误信息

--- src/components/common/order/order-push-status.vue
+++ src/components/common/order/order-push-status.vue
@@ -1,9 +1,7 @@
 <template>
   <sapn>
     <el-tag v-if="status === '1'" type="success">推送完成</el-tag>
-    <span v-else-if="status === '2'" class="text-danger">
-      {{msg ? JSON.parse(msg).msg : '推送失败'}}
-    </span>
+    <el-tag v-else-if="status === '2'" type="danger">推送失败</el-tag>
     <el-tag v-else-if="status === '0'">推送中</el-tag>
   </sapn>
 </template>
