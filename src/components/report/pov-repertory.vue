db7feb7be72f5eb24b59083fcb50dd0dd2c66ca1
周浩 <798804880@qq.com>
2020-07-22 11:26:46 +0800

    Revert "[新增]接种单位疫苗盘点表新增疫苗种类筛选项"
    This reverts commit d0d386b1

--- src/components/report/pov-repertory.vue
+++ src/components/report/pov-repertory.vue
@@ -27,16 +27,8 @@
                 </el-col>
               </oms-form-row>
             </el-col>
-            <el-col :span="8">
-              <oms-form-row :span="7" label="疫苗种类">
-                <el-select placeholder="请选择疫苗种类" type="text" v-model="searchWord.type">
-                  <el-option :key="item.key" :label="item.label" :value="item.key"
-                             v-for="item in vaccineSignList"></el-option>
-                </el-select>
-              </oms-form-row>
-            </el-col>
-            <el-col :span="8">
-              <oms-form-row :span="4" label="">
+            <el-col :span="6">
+              <oms-form-row :span="2" label="">
                 <perm label="pov-stock-form-manager-export">
                   <el-button type="primary" @click="search" :disabled="isLoading">
                     查询
@@ -124,8 +116,7 @@
         showSearch: true,
         searchWord: {
           startTime: '',
-          endTime: '',
-          type: ''
+          endTime: ''
         },
         bizDateAry: '',
         isLoading: false
@@ -134,9 +125,6 @@
     computed: {
       type() {
         return this.$route.meta.type;
-      },
-      vaccineSignList() {
-        return this.$getDict('vaccineSign');
       },
       getHeight: function () {
         return parseInt(this.$store.state.bodyHeight, 10) - 70 + this.fixedHeight;
@@ -169,7 +157,7 @@
         }
         this.searchWord.startTime = this.$formatAryTime(this.bizDateAry, 0);
         this.searchWord.endTime = this.$formatAryTime(this.bizDateAry, 1);
-        let params = Object.assign({}, this.searchWord);
+        let params = Object.assign({}, this.searchWord, {type: this.type - 1});
         this.isLoading = true;
         this.$store.commit('initPrint', {
           isPrinting: true,
@@ -238,7 +226,7 @@
         }
         this.searchWord.startTime = this.$formatAryTime(this.bizDateAry, 0);
         this.searchWord.endTime = this.$formatAryTime(this.bizDateAry, 1);
-        let params = Object.assign({}, this.searchWord);
+        let params = Object.assign({}, this.searchWord, {type: this.type - 1});
         this.loadingData = true;
         this.$http.get('/erp-statement/pov-stock', {params}).then(res => {
           res.data.forEach(i => {
@@ -252,8 +240,7 @@
       resetSearchForm: function () {
         this.searchWord = {
           startTime: '',
-          endTime: '',
-          type: ''
+          endTime: ''
         };
         this.bizDateAry = '';
       },
