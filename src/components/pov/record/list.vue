f36f7bbff3c046157da51f07f57307b3a7d81f13
周浩 <798804880@qq.com>
2020-08-25 16:15:16 +0800

    [优化]vss注射记录，增加批号查询功能

--- src/components/pov/record/list.vue
+++ src/components/pov/record/list.vue
@@ -30,23 +30,54 @@
                            v-model="searchCondition.vaccineId" popper-class="good-selects"
                            @click.native.once="searchProduct('')">
                   <el-option :value="org.id" :key="org.id" :label="org.name"
-                             v-for="org in orgList">
+                             v-for="org in detailList">
                     <div style="overflow: hidden">
                       <span class="pull-left">
-                        {{org.name}}
+                        {{ org.name }}
                       </span>
                     </div>
                     <div style="overflow: hidden">
                       <span class="select-other-info pull-left">
-                        <span v-show="org.specifications">规格:</span>{{org.specifications}}
+                        <span v-show="org.specifications">规格:</span>{{ org.specifications }}
                       </span>
                       <span class="select-other-info pull-left">
-                        <span v-show="org.code">货品编号:</span>{{org.code}}
+                        <span v-show="org.code">货品编号:</span>{{ org.code }}
                       </span>
                       <span class="select-other-info pull-left"><span
                         v-show="org.factoryName">生产单位:</span>{{ org.factoryName }}
                       </span>
                     </div>
+                  </el-option>
+                </el-select>
+              </oms-form-row>
+            </el-col>
+            <el-col :span="8">
+              <oms-form-row :span="5" label="批号">
+                <el-select filterable remote placeholder="请输入关键字搜索批号" :remote-method="queryBatchNumbers"
+                           :clearable="true"
+                           v-model="searchCondition.batchNumberId" popper-class="good-selects"
+                           @click.native.once="queryBatchNumbers('')">
+                  <el-option :value="item.id" :key="item.id" :label="item.batchNumber"
+                             v-for="item in batchNumberList">
+                    <div style="overflow: hidden">
+                      <span class="pull-left">
+                        {{ item.batchNumber }}
+                      </span>
+                    </div>
+                    <!--                    <div style="overflow: hidden">-->
+                    <!--                      <span class="select-other-info pull-left">-->
+                    <!--                        <span v-show="item.goodsName">疫苗主档名称:</span> {{ item.goodsName }}-->
+                    <!--                      </span>-->
+                    <!--                      <span class="select-other-info pull-left">-->
+                    <!--                        <span v-show="item.goodsCode">疫苗编号:</span>{{ item.goodsCode }}-->
+                    <!--                      </span>-->
+                    <!--                      <span class="select-other-info pull-left">-->
+                    <!--                        <span v-show="item.specification">规格:</span>{{ item.specification }}-->
+                    <!--                      </span>-->
+                    <!--                      <span class="select-other-info pull-left"><span-->
+                    <!--                        v-show="item.orgName">生产单位:</span>{{ item.orgName }}-->
+                    <!--                      </span>-->
+                    <!--                    </div>-->
                   </el-option>
                 </el-select>
               </oms-form-row>
@@ -104,7 +135,7 @@
           <div class="order-list-item order-list-item-bg" v-for="item in CDCs" :key="">
             <el-row>
               <el-col :span="4" class="R pt10">
-                {{ item.actualTime | minute}}
+                {{ item.actualTime | minute }}
                 <div>
                   <el-tooltip class="item" effect="dark" content="登记编号" placement="right">
                     <span class="font-gray">{{ item.inoculatorNumber }}</span>
@@ -122,7 +153,7 @@
                 </div>
                 <div>
                   <el-tooltip class="item" effect="dark" content="生产单位" placement="right">
-                    <span class="font-gray">{{item.origin}}</span>
+                    <span class="font-gray">{{ item.origin }}</span>
                   </el-tooltip>
                 </div>
               </el-col>
@@ -130,10 +161,10 @@
                 {{ item.batchNumber }}
               </el-col>
               <el-col :span="6" class="R pt10">
-                {{ item.actualCode}}
+                {{ item.actualCode }}
               </el-col>
               <el-col :span="2">
-                {{item.sourceSystem }}
+                {{ item.sourceSystem }}
                 <el-tag type="warning" v-show="item.traceCodeId==='NULL'">未知追溯码</el-tag>
               </el-col>
             </el-row>
@@ -152,128 +183,145 @@
   </div>
 </template>
 <script>
-  import utils from '@/tools/utils';
+import utils from '@/tools/utils';
 
-  export default {
-    data() {
-      return {
-        loadingData: true,
-        showSearch: true,
-        isLoading: false,
-        CDCs: [],
-        filters: {
-          actualStartTime: '',
-          actualEndTime: '',
-          vaccineId: '',
-          actualCode: '',
-          traceCodeId: '',
-          checked: false
-        },
-        searchCondition: {
-          actualStartTime: '',
-          actualEndTime: '',
-          vaccineId: '',
-          actualCode: '',
-          traceCodeId: '',
-          checked: false
-        },
-        actualTime: '',
-        orgList: [], // 货主列表,
-        pager: {
-          currentPage: 1,
-          count: 0,
-          pageSize: 15
-        },
-        doing: false
-      };
-    },
-    mounted() {
-      this.getRecordPage(1);
-    },
-    watch: {
+export default {
+  data() {
+    return {
+      loadingData: true,
+      showSearch: true,
+      isLoading: false,
+      CDCs: [],
       filters: {
-        handler: function () {
-          this.getRecordPage(1);
-        },
-        deep: true
-      }
-    },
-    methods: {
-      checkChange(val) {
-        if (val) {
-          this.searchCondition.traceCodeId = 'NULL';
-        } else {
-          this.searchCondition.traceCodeId = '';
-        }
+        actualStartTime: '',
+        actualEndTime: '',
+        vaccineId: '',
+        actualCode: '',
+        traceCodeId: '',
+        checked: false
       },
-      handleSizeChange(val) {
-        this.pager.pageSize = val;
+      searchCondition: {
+        actualStartTime: '',
+        actualEndTime: '',
+        vaccineId: '',
+        actualCode: '',
+        traceCodeId: '',
+        checked: false,
+        batchNumberId: ''
+      },
+      actualTime: '',
+      detailList: [], // 货主列表,
+      pager: {
+        currentPage: 1,
+        count: 0,
+        pageSize: 15
+      },
+      doing: false,
+      batchNumberList: []
+    };
+  },
+  mounted() {
+    this.getRecordPage(1);
+  },
+  watch: {
+    filters: {
+      handler: function () {
         this.getRecordPage(1);
       },
-      handleCurrentChange(val) {
-        this.getRecordPage(val);
-      },
-      getRecordPage(pageNo) { // 得到波次列表
-        this.pager.currentPage = pageNo;
-        let params = Object.assign({
-          pageNo: pageNo,
-          pageSize: this.pager.pageSize
-        }, this.filters);
-        this.loadingData = true;
-        this.$http.get('/injection-task/injection-task-query', {params}).then(res => {
-          this.CDCs = res.data.list;
-          this.pager.count = res.data.count;
-          this.loadingData = false;
-        });
-      },
-      searchProduct(query) {
-        let params = Object.assign({}, {
-          deleteFlag: false,
-          keyWord: query
-        });
-        this.$http.get('/goods/main-info-pager', {params}).then(res => {
-          this.orgList = res.data.list;
-        });
-      },
-      searchInOrder: function () {// 搜索
-        this.searchCondition.actualStartTime = this.$formatAryTime(this.actualTime, 0);
-        this.searchCondition.actualEndTime = this.$formatAryTime(this.actualTime, 1);
-        Object.assign(this.filters, this.searchCondition);
-      },
-      resetSearchForm: function () {// 重置表单
-        let temp = {
-          actualStartTime: '',
-          actualEndTime: '',
-          vaccineId: '',
-          actualCode: '',
-          traceCodeId: '',
-          checked: false
-        };
-        this.actualTime = '';
-        Object.assign(this.searchCondition, temp);
-        Object.assign(this.filters, temp);
-      },
-      exportFile: function () {// 导出表单
-        this.searchCondition.actualStartTime = this.$formatAryTime(this.actualTime, 0);
-        this.searchCondition.actualEndTime = this.$formatAryTime(this.actualTime, 1);
-        let params = Object.assign({}, this.searchCondition);
-        this.isLoading = true;
-        this.$store.commit('initPrint', {isPrinting: true, moduleId: '/pov'});
-        this.$http.get('/injection-task/export', {params}).then(res => {
-          utils.download(res.data.path, '疫苗注射记录表');
-          this.isLoading = false;
-          this.$store.commit('initPrint', {isPrinting: false, moduleId: '/pov'});
-        }).catch(error => {
-          this.isLoading = false;
-          this.$store.commit('initPrint', {isPrinting: false, moduleId: '/pov'});
-          this.$notify.error({
-            message: error.response && error.response.data && error.response.data.msg || '导出失败'
-          });
-        });
-      },
-      formatTime: function (date) {
-        return date ? this.$moment(date).format('YYYY-MM-DD') : '';
-      }
+      deep: true
     }
-  };
+  },
+  methods: {
+    queryBatchNumbers(query) {
+      if (!this.searchCondition.vaccineId) {
+        this.$notify.info('请先选择疫苗主档');
+        return;
+      }
+      const params = {
+        keyWord: query,
+        goodsId: this.searchCondition.vaccineId
+      };
+      this.$http.get('/batch-number/pager', {params}).then(res => {
+        this.batchNumberList = res.data.list;
+      });
+    },
+    checkChange(val) {
+      if (val) {
+        this.searchCondition.traceCodeId = 'NULL';
+      } else {
+        this.searchCondition.traceCodeId = '';
+      }
+    },
+    handleSizeChange(val) {
+      this.pager.pageSize = val;
+      this.getRecordPage(1);
+    },
+    handleCurrentChange(val) {
+      this.getRecordPage(val);
+    },
+    getRecordPage(pageNo) { // 得到波次列表
+      this.pager.currentPage = pageNo;
+      let params = Object.assign({
+        pageNo: pageNo,
+        pageSize: this.pager.pageSize
+      }, this.filters);
+      this.loadingData = true;
+      this.$http.get('/injection-task/injection-task-query', {params}).then(res => {
+        this.CDCs = res.data.list;
+        this.pager.count = res.data.count;
+        this.loadingData = false;
+      });
+    },
+    searchProduct(query) {
+      let params = Object.assign({}, {
+        deleteFlag: false,
+        keyWord: query
+      });
+      this.$http.get('/goods/main-info-pager', {params}).then(res => {
+        this.detailList = res.data.list;
+      });
+    },
+    searchInOrder: function () {// 搜索
+      this.searchCondition.actualStartTime = this.$formatAryTime(this.actualTime, 0);
+      this.searchCondition.actualEndTime = this.$formatAryTime(this.actualTime, 1);
+      Object.assign(this.filters, this.searchCondition);
+    },
+    resetSearchForm: function () {// 重置表单
+      let temp = {
+        actualStartTime: '',
+        actualEndTime: '',
+        vaccineId: '',
+        actualCode: '',
+        traceCodeId: '',
+        checked: false,
+        batchNumberId: ''
+      };
+      this.batchNumberList = [];
+      this.actualTime = '';
+      Object.assign(this.searchCondition, temp);
+      Object.assign(this.filters, temp);
+    },
+    exportFile: function () {// 导出表单
+      this.searchCondition.actualStartTime = this.$formatAryTime(this.actualTime, 0);
+      this.searchCondition.actualEndTime = this.$formatAryTime(this.actualTime, 1);
+      let params = Object.assign({}, this.searchCondition);
+      this.isLoading = true;
+      this.$store.commit('initPrint', {isPrinting: true, moduleId: '/pov'});
+      this.$http.get('/injection-task/export', {params}).then(res => {
+        utils.download(res.data.path, '疫苗注射记录表');
+        this.isLoading = false;
+        this.$store.commit('initPrint', {isPrinting: false, moduleId: '/pov'});
+      }).catch(error => {
+        this.isLoading = false;
+        this.$store.commit('initPrint', {isPrinting: false, moduleId: '/pov'});
+        this.$notify.error({
+          message: error.response && error.response.data && error.response.data.msg || '导出失败'
+        });
+      });
+    },
+    formatTime: function (date) {
+      return date ? this.$moment(date).format('YYYY-MM-DD') : '';
+    }
+  }
+};
 </script>
